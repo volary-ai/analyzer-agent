d7706ae5028037924c19673634b6f6a033a1b8c0
Peter Ebden <peter@volary.ai>
2025-12-02 14:05:01 +0000

    format

--- src/volary_analyzer/test/test_print_issues.py
+++ src/volary_analyzer/test/test_print_issues.py
@@ -5,21 +5,20 @@ from ..print_issues import render_summary_markdown
 
 
 class TestRenderSummaryMarkdown:
-
     def test_no_eval(self):
         """Test that we can successfully render something on non-evaluated issues."""
         with open("src/volary_analyzer/test/testdata/please-issues.json") as f:
             analysis = TechDebtAnalysis.model_validate_json(f.read())
         md = render_summary_markdown(analysis)
-        assert md.startswith('|')
-        assert md.endswith('|')
-        assert 'Evaluation' not in md
+        assert md.startswith("|")
+        assert md.endswith("|")
+        assert "Evaluation" not in md
 
     def test_eval(self):
         """Test that we can successfully render something on evaluated issues."""
         with open("src/volary_analyzer/test/testdata/please-issues-evaluated.json") as f:
             analysis = EvaluatedTechDebtAnalysis.model_validate_json(f.read())
         md = render_summary_markdown(analysis)
-        assert md.startswith('|')
-        assert md.endswith('|')
-        assert 'Evaluation' in md
+        assert md.startswith("|")
+        assert md.endswith("|")
+        assert "Evaluation" in md
